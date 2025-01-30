d9639eb4572afd5561d04110b13da72e6f46baf8
Esiru <evenstetrud@gmail.com>
2025-01-30 13:36:26 +0100

    FEAT: added initial routing and encounter-list for landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { EncounterListComponent } from "./encounter-list/encounter-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: "", component: EncounterListComponent }];

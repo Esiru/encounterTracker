a9681d86094f1934a04959cf66d5ccd69a33b68c
Esiru <evenstetrud@gmail.com>
2025-01-31 09:57:52 +0100

    FEAT: implemented keycloak login, with build-specific id

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { provideKeycloak } from "keycloak-angular";
+import { environment } from "../environments/environment.development";
 
-import { routes } from './app.routes';
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideKeycloak({
+      config: {
+        url: "https://lemur-18.cloud-iam.com/auth",
+        realm: "encountertracker",
+        clientId: environment.keycloakClientId,
+      },
+      initOptions: {
+        onLoad: "login-required",
+        flow: "standard",
+      },
+    }),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  keycloakClientId: "https://esiru.github.io/encounterTracker/",
+};
